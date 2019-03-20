b4cb54c7b8f435d075ee27185082724c7387571b
Lucas <lucas.sanner@codalia.net>
2019-03-20 10:10:36 +0000

    Cleans and comments code in the content plugin.

--- plugins/content/okeydoc/okeydoc.php
+++ plugins/content/okeydoc/okeydoc.php
@@ -1,20 +1,23 @@
 <?php
 /**
  * @package Okey DOC 2
- * @copyright Copyright (c) 2018 - 2018 Lucas Sanner
+ * @copyright Copyright (c) 2015 - 2019 Lucas Sanner
  * @license GNU General Public License version 3, or later
  */
 
 // No direct access
 defined('_JEXEC') or die('Restricted access');
-// Import the JPlugin class
-jimport('joomla.plugin.plugin');
 
 
+/**
+ * The Okey DOC 2 content plugin.
+ *
+ */
 class plgContentOkeydoc extends JPlugin
 {
   protected $post;
   protected $jform;
+
 
   /**
    * Constructor.
@@ -39,32 +42,34 @@ class plgContentOkeydoc extends JPlugin
   }
 
 
-  public function onContentPrepare($context, &$data, &$params, $page)
+  /**
+   * The display event.
+   *
+   * @param   string    $context     The context
+   * @param   stdClass  $item        The item
+   * @param   Registry  $params      The params
+   * @param   integer   $limitstart  The start
+   *
+   * @return  string
+   *
+   * @since   3.7.0
+   */
+  public function onContentAfterDisplay($context, &$item, &$params, $limitstart)
   {
   }
 
 
-  public function onContentAfterTitle($context, &$data, &$params, $limitstart)
-  {
-  }
-
-
-  public function onContentBeforeDisplay($context, &$data, &$params, $limitstart)
-  {
-  }
-
-
-  public function onContentAfterDisplay($context, &$data, &$params, $limitstart)
-  {
-  }
-
-
-  public function onContentBeforeSave($context, $data, $isNew)
-  {
-    return true;
-  }
-
-
+  /**
+   * Updates internal and external document linkings.
+   *
+   * @param   string   $context  The context of the content passed to the plugin (added in 1.6)
+   * @param   object   $data     A JTableContent object
+   * @param   boolean  $isNew    If the content is just about to be created
+   *
+   * @return  void     Result will be omitted.
+   *
+   * @since   1.6
+   */
   public function onContentAfterSave($context, $data, $isNew)
   {
     if($context == 'com_okeydoc.document' || $context == 'com_okeydoc.form') {
@@ -133,9 +138,21 @@ class plgContentOkeydoc extends JPlugin
   }
 
 
+  /**
+   * Don't allow a document to be deleted if articles or article categories contain links to
+   * this document.
+   *
+   * @param   string  $context  The context for the content passed to the plugin.
+   * @param   object  $data     The data relating to the content that was deleted.
+   *
+   * @return  boolean
+   *
+   * @since   1.6
+   */
   public function onContentBeforeDelete($context, $data)
   {
     if($context == 'com_okeydoc.document') {
+      // Checks that the document is not linked from an article or an article category.
       $model = JModelLegacy::getInstance('Document', 'OkeydocModel');
       $linkinks = $model->getExternalDocumentLinkings($data->id);
 
@@ -149,51 +166,57 @@ class plgContentOkeydoc extends JPlugin
   }
 
 
+  /**
+   * Removes all the rows relating to the deleted document in the mapping tables.
+   * Checks for deleted articles or article categories and removes the document linkings
+   * accordingly..
+   *
+   * @param   string  $context  The context for the content passed to the plugin.
+   * @param   object  $data     The data relating to the content that was deleted.
+   *
+   * @return  void     Result will be omitted.
+   *
+   * @since   1.6
+   */
   public function onContentAfterDelete($context, $data)
   {
     if($context == 'com_okeydoc.document') {
-      // 
+      // Removes the document linkings relating to this document.
       $this->deleteDocumentLinkings($data->id);
 
-      //
+      // Deletes the archived file data binds to this document.
       $db = JFactory::getDbo();
       $query = $db->getQuery(true);
-
       $query->delete('#__okeydoc_archive')
 	    ->where('doc_id='.(int)$data->id);
       $db->setQuery($query);
       $db->execute();
     }
     elseif($context == 'com_content.article') {
+      // Searches links to any document in the article text.
       $text = $data->introtext.$data->fulltext;
       $docIds = $this->searchDocumentLinks($text);
+      // Removes all linkings relating to this article from the linking table.
       $this->deleteDocumentLinkings($docIds, $data->id, 'article', 'external');
     }
     elseif($context == 'com_categories.category' && $data->extension == 'com_content') {
+      // Same for the article category text.
       $docIds = $this->searchDocumentLinks($data->description);
       $this->deleteDocumentLinkings($docIds, $data->id, 'category', 'external');
     }
   }
 
 
-  public function onContentPrepareForm($form, $data)
-  {
-    return true;
-  }
-
-
-  public function onContentPrepareData($context, $data)
-  {
-    return true;
-  }
-
-
-  public function onContentChangeState($context, $pks, $value)
-  {
-    return true;
-  }
-
-
+  /**
+   * Utility function which deletes rows in the #__okeydoc_document_linking table according to the given parameters.
+   *
+   * @param   array    $docIds        The ids of the concerned documents.
+   * @param   integer  $itemId        The id of the linked item.
+   * @param   string   $itemType      The type of the linked item (article or category).
+   * @param   string   $linkingType   The type of linkink to remove (internal or external).
+   *
+   * @return  void     
+   */
   private function deleteDocumentLinkings($docIds = null, $itemId = null, $itemType = null, $linkingType = null)
   {
     // Prevents the complete deletion of the rows as well as the request to fail. 
@@ -208,7 +231,7 @@ class plgContentOkeydoc extends JPlugin
 
     $db = JFactory::getDbo();
     $query = $db->getQuery(true);
-    // 
+    // Builds the query according to the given parameters. 
     $query->delete('#__okeydoc_document_linking');
 
     if($docIds !== null) {
@@ -232,6 +255,13 @@ class plgContentOkeydoc extends JPlugin
   }
 
 
+  /**
+   * Utility function which inserts rows in the #__okeydoc_document_linking table according to the given values.
+   *
+   * @param   array    $values   The values to insert into the table.
+   *
+   * @return  void     
+   */
   private function insertDocumentLinkings($values)
   {
     $db = JFactory::getDbo();
@@ -246,6 +276,13 @@ class plgContentOkeydoc extends JPlugin
   }
 
 
+  /**
+   * Parses the given string and searches for links leading to documents.
+   *
+   * @param   string   $text     The string to search into.
+   *
+   * @return  array              The document ids found into the given string.     
+   */
   public function searchDocumentLinks($text)
   {
     $docIds = array();
